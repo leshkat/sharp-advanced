d35d882a6e08ecadacef3712259140c9b75a7bf0
Alona <126986191+leshkat@users.noreply.github.com>
2024-10-07 15:59:09 +0300

    Add files via upload

---
+++ hw 3 (1)/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public interface IOutput
+{
+    void Show();
+    void Show(string info);
+}
+
+public class MyArray : IOutput
+{
+    private int[] array;
+
+    public MyArray(int[] array)
+    {
+        this.array = array;
+    }
+
+    public void Show()
+    {
+        Console.WriteLine("Елементи масиву: ");
+        foreach (int item in array)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+
+    public void Show(string info)
+    {
+        Console.WriteLine(info);
+        Show();
+    }
+}
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        int[] numbers = { 10, 20, 30};
+        MyArray myArray = new MyArray(numbers);
+
+        myArray.Show();
+        myArray.Show("Просто інформаційне повідомлення");
+    }
+}
